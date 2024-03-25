148530a838a78bdba801b7f55b6efb57e16ef204
Adetoke0987 <awesometokeh@gmail.com>
2024-03-25 07:39:49 +0100

    update

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,28 +1,55 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
+const Form = () => {
+    const [userName, SetUsername] = useState("");
+    const [email, SetEmail] = useState("");
+    const [password, SetPassword] = useState("");
+    const [error, SetError] = useState({});
 
-const Form = ()=>{
-
-    const [userName, SetUsername] = useState("")
-    const [email, SetEmail] = useState("")
-    const [password, SetPassword] = useState("")
+    useEffect(() => {
+        
+        if (Object.keys(error).length === 0) {
+            SetEmail("");
+            SetUsername("");
+            SetPassword("");
+        }
+    }, [error]); 
 
     function formHandler(e) {
-        e.preventDefault()
-        SetEmail("")
-        SetUsername("")
-        SetPassword("")
+        e.preventDefault();
+
+        let errorObj = {};
+
+        if (userName.trim() === "") {
+            errorObj.userName = "Name required";
+        }
+        if (email.trim() === "") {
+            errorObj.email = "Email required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errorObj.email = "Email invalid";
+        }
+        if (password.trim() === "") {
+            errorObj.password = "Password is required";
+        } else if (password.length < 6) {
+            errorObj.password = "Password must be at least 6 characters long";
+        }
+
+        SetError(errorObj); 
     }
 
-    return(
+    return (
         <>
             <div>
-                <form onSubmit={formHandler}>
-                    <input type="text" placeholder="Name" value={userName} onChange={(e)=> SetUsername(e.target.value)}/> <br />
-                    <input type="email" placeholder="Email" value={email} onChange={(e)=>SetEmail(e.target.value)}/> <br />
-                    <input type="password" placeholder="Password" value={password} onChange={(e)=>SetPassword(e.target.value)}/> <br />
-                    <button type="submit">SUBMIT</button>
-                </form>
+            <form onSubmit={formHandler}>
+                <input type="text" placeholder="Name" value={userName} onChange={(e) => SetUsername(e.target.value)} /> <br />
+                {error.userName && <div className="error-message">{error.userName}</div>} 
+                <input type="email" placeholder="Email" value={email} onChange={(e) => SetEmail(e.target.value)} /> <br />
+                {error.email && <div className="error-message">{error.email}</div>} 
+                <input type="password" placeholder="Password" value={password} onChange={(e) => SetPassword(e.target.value)} /> <br />
+                {error.password && <div className="error-message">{error.password}</div>} 
+                <button type="submit">SUBMIT</button>
+            </form>
+
                 <div>
                     <div>{userName}</div>
                     <div>{email}</div>
@@ -30,6 +57,7 @@ const Form = ()=>{
                 </div>
             </div>
         </>
-    )
-}
-export default Form
+    );
+};
+
+export default Form;
